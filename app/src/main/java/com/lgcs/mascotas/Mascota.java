faea199d42fe67253415fc98216c12bc507f95ce
Luisg_123 <luisg_123@hotmail.com>
2016-07-14 21:10:51 -0600

    Iniciacion

---
+++ app/src/main/java/com/lgcs/mascotas/Mascota.java
@@ -0,0 +1,50 @@
+package com.lgcs.mascotas;
+
+/**
+ * Created by Tecnologías on 07/07/2016.
+ */
+public class Mascota {
+    private String nombre;
+    private int foto;
+    private int megusta;
+    private String raza;
+
+    public Mascota(String nombre, int foto) {
+        this.nombre = nombre;
+        this.foto = foto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+
+    public int getMegusta() {
+        return megusta;
+    }
+
+    public void setMegusta(int megusta) {
+        this.megusta = megusta;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+
+}
