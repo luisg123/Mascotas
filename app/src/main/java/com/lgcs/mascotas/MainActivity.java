ee2f7004d00a0fc6d43ca2d6025f53957df9a986
Luisg_123 <luisg_123@hotmail.com>
2016-07-19 14:35:46 -0600

    ActionBar

--- app/src/main/java/com/lgcs/mascotas/MainActivity.java
+++ app/src/main/java/com/lgcs/mascotas/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import java.util.ArrayList;
 
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar miActionBar = (Toolbar) findViewById(R.id.miActionBar);
+        setSupportActionBar(miActionBar);
 
         listaMascotas = (RecyclerView) findViewById(R.id.rvMascotas);
 
